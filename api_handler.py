89f38d5ce445d229ac57fe88d591e3f7b78543a7
Maf313 <156673680+Maf313@users.noreply.github.com>
2024-01-21 21:06:29 +0330

    Add files via upload

---
+++ api_handler.py
@@ -0,0 +1,24 @@
+from requests import Response, get, post
+
+
+def _get_slots() -> dict[str, int]:
+    responce: Response = get(url="http://localhost/slots")
+    return responce.json()
+
+
+def _reserve(id: str, amount: int):
+    response: Response = post(
+        url="http://localhost/reserve", json={"id": id, "reserved": amount})
+    return response.json()["success"]
+
+
+def get_reservation_amount(id: str) -> int:
+    return _get_slots()[id]
+
+
+def set_reservation_amount(id: str, amount: int):
+    return _reserve(id=id, amount=_get_slots()[id]-amount)
+
+
+def make_reservation(id: str):
+    return _reserve(id, 1)
