ea51920149dde26ccc364d43066c82cf3f864a1d
Maf313 <156673680+Maf313@users.noreply.github.com>
2024-01-22 04:25:14 +0330

    Classes changes

---
+++ clinic.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Boolean, Column, String
+from sqlalchemy.orm import relationship, mapped_column, Mapped
+from .base import SQLclass
+
+
+class Clinic(SQLclass):
+    __tablename__ = "clinics"
+    id: Mapped[str] = mapped_column(String, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    address: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    services: Mapped[str] = mapped_column(String)
+    is_available: Mapped[str] = mapped_column(Boolean)
+
+    appointments = relationship(
+        argument="Appointment", back_populates="clinic")
+    managers = relationship(argument="Manager", back_populates="clinic")
